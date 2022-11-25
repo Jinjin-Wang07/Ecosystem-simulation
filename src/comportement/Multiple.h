5756ec7225332eeac190f47d59a88f4474a90cc1
Ni Qiang <ni.qiang@imt-atlantique.net>
2022-11-25 15:20:54 +0100

    put headers in comportement folder

---
+++ src/comportement/Multiple.h
@@ -0,0 +1,15 @@
+#include "Comportement.h"
+#include "Kamikaze.h"
+#include "Prevoyante.h"
+#include "Gregaire.h"
+#include "Peureuse.h"
+#include <vector>
+using namespace std;
+class Multiple : public IComportement{
+private:
+    vector<IComportement> comportements;
+public:
+    Multiple();
+    ~Multiple();
+    void move();
+};
---
+++ src/comportement/Prevoyante.h
@@ -0,0 +1,12 @@
+#include "Comportement.h"
+#include <vector>
+using namespace std;
+typedef vector<float> Path;
+class Prevoyante {
+private:
+vector<Path> getNeighborPaths(vector<Bestiole> const& neighbors);
+public:
+Prevoyante();
+~Prevoyante();
+void move();
+};
